45cfe1c6876a85efd6d557c0a257dbac74282d12
Rene Alas <rene.alaslopez@telus.com>
2024-08-26 08:57:13 -0600

    Added the navigation, app code and left only the code of the pages to be added for testing

---
+++ screens/AddTaskScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+export default function AddTaskScreen({ route, navigation }) {
+
+  return (
+    <View style={styles.container}>
+      {/* Here Add your Code to add Tasks */}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16
+  },
+})
---
+++ screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+export default function HomeScreen({ navigation }) {
+
+  return (
+    <View style={styles.container}>
+      {/* Here add your Code for Task List and button to navigate to add new tasks, also remember todo's should be able to be deleted. */}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16
+  },
+})
