94a54ecf42fde49ae519de7c395801458d8472eb
jmejiamu <jmejiamu@gmail.com>
2024-09-30 10:59:55 -0700

    feat(todo): todo app

--- screens/AddTaskScreen.js
+++ screens/AddTaskScreen.js
@@ -1,18 +1,32 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, Button } from "react-native";
+import { todoItem } from "./HomeScreen";
 
 export default function AddTaskScreen({ route, navigation }) {
+  const [newTask, setNewTack] = useState("");
 
+  const onHandleSubmit = () => {
+    const id = new Date();
+    todoItem.push([{ id: id, text: newTask }]);
+    navigation.navigate("Home");
+  };
+  console.log(todoItem);
   return (
     <View style={styles.container}>
       {/* Here Add your Code to add Tasks */}
+      <TextInput
+        placeholder="Enter new task"
+        value={newTask}
+        onChangeText={setNewTack}
+      />
+      <Button title="Add Task" onPress={onHandleSubmit} />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16
+    padding: 16,
   },
-})
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Button, FlatList } from "react-native";
 
+export const todoItem = [
+  {
+    id: 1,
+    text: "clean",
+  },
+  {
+    id: 2,
+    text: "pay bills",
+  },
+];
 export default function HomeScreen({ navigation }) {
-
+  const [data, setData] = useState(todoItem);
+  const onHandleDelete = (id) => {
+    setData(todoItem.filter((item) => item.id !== id));
+  };
   return (
     <View style={styles.container}>
       {/* Here add your Code for Task List and button to navigate to add new tasks, also remember todo's should be able to be deleted. */}
+      <FlatList
+        data={data}
+        renderItem={(item) => {
+          return (
+            <View style={{}}>
+              <Text>{item.item.text}</Text>
+              <Button
+                title="Delete"
+                onPress={() => onHandleDelete(item.item.id)}
+              />
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.id}
+      />
+      <Button
+        title="Add new Task"
+        onPress={() => navigation.navigate("AddTask")}
+      />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16
+    padding: 16,
   },
-})
+});
